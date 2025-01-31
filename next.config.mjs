748b6a4187ae3f03e2475989a004645b2ecdef36
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-31 23:51:09 +0530

    config

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bayut-production.s3.eu-central-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
