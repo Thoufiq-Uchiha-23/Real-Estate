94d63f0b2269254557a415fd225782b3393a7b03
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-02-02 23:29:01 +0530

    app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import "../styles/globals.css";
+import Router from "next/router";
+import Head from "next/head";
+import NProgress from "nprogress";
 import { Provider } from "../components/ui/provider.jsx";
+
+import Layout from "../components/Layout";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head></Head>
+
+      <Provider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </>
   );
 }
