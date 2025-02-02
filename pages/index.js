d893abfd8789646ab09c5b8fbf9640f68e07143c
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-02-02 23:29:08 +0530

    index

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,49 @@ import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 import { time } from "framer-motion";
 
-const Banner = ({purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl}) => (
+import Property from "../components/Property";
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+
+const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  linkName,
+  imageUrl,
+}) => (
   <Flex flexwrap="wrap" justifyContent="center" alignItems="center" m={10}>
-    <Image src={imageUrl} width={500} height={300} alt="banner"/>
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
     <Box p={5}>
-      <Text color={"gray.500"} fontSize={"sm"} fontWeight={"medium"}>{purpose}</Text>
-      <Text fontSize={"3xl"} fontWeight={"bold"}>{title1}<br/>{title2}</Text>
-      <Text fontSize={"lg"} paddingTop={"3"} paddingBottom={"3"} color={"gray.700"}>{desc1}<br/>{desc2}</Text>
+      <Text color={"gray.500"} fontSize={"sm"} fontWeight={"medium"}>
+        {purpose}
+      </Text>
+      <Text fontSize={"3xl"} fontWeight={"bold"}>
+        {title1}
+        <br />
+        {title2}
+      </Text>
+      <Text
+        fontSize={"lg"}
+        paddingTop={"3"}
+        paddingBottom={"3"}
+        color={"gray.700"}
+      >
+        {desc1}
+        <br />
+        {desc2}
+      </Text>
       <Button fontSize={"xl"} bg={"blue.300"} color={"white"}>
         <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
   </Flex>
-)
-export default function Home() {
+);
+export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <Box>
       <Banner
         purpose="RENT A HOME"
         title1={"Rental Homes for"}
@@ -28,8 +54,14 @@ export default function Home() {
         desc2={"and more"}
         buttonText={"Explore Renting"}
         linkName={"/search?purpose=for-rent"}
+        width={"auto"}
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
+      <Flex flexWrap="wrap">
+        {propertiesForRent.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
       <Banner
         purpose="BUY A HOME"
         title1={"Find, Buy & Own Your"}
@@ -40,6 +72,27 @@ export default function Home() {
         linkName={"/search?purpose=for-sale"}
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-    </div>
-  )
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+    </Box>
+  );
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
+  );
+  const propertyForRent = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
+  );
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
