66af7cc455130f6c4c28a1e92d93d7b2e92675ab
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-02-04 22:49:58 +0530

    error error error

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,14 +1,51 @@
 import { useEffect, useState } from "react";
-import { Flex, Select, Box, Text, Input, Spinner, Icon,Button } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+  SelectRoot,
+} from "@chakra-ui/react";
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "@/components/ui/select";
+// import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 
+import { filterData, getFilterValues } from "../utils/filterdata";
+
 const SearchFilters = () => {
-    return (
-        <div>
-            searchFilters
-        </div>
-    )
-}
+  const [filters, setFilters] = useState(filterData);
+
+  const searchProperties = (filterValues) => {};
+
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <SelectRoot
+            collection={filter.queryName}
+            placeholder={filter.placeholder}
+            w="fit-content"
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            <SelectContent></SelectContent>
+          </SelectRoot>
+        </Box>
+      ))}
+    </Flex>
+  );
+};
 
 export default SearchFilters;
